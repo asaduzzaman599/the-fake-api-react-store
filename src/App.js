62f4398cb36443015ab2df1c44ab0298c2503721
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-18 21:45:29 +0600

    animation added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
 import Products from './Components/Products/Products';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +13,10 @@ function App() {
   const changeCount = ()  =>{
     setCount(count+1)
   }
+
+  useEffect(()=>{
+    AOS.init();
+  },[])
   return (
     <div className="App">
       <Header count={count}></Header>
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -5,7 +5,7 @@ const Product = ({product,count}) => {
     // console.log(count)
     const {title,image,description} = product;
     return (
-        <div className='col-md-4'>
+        <div data-aos="fade-up" className='col-md-4'>
             <div className="card p-3 border h-100 justify-content-between">
             <div>
             <img  style={{width:"100%",height:"200px"}} src={image} alt="" />
